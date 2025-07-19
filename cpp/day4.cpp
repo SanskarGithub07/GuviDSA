f3180f11b877d1394b88656e7fa727d9250dc42a
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2025-07-19 14:03:49 +0530

    Cpp Day-4

---
+++ cpp/day4.cpp
@@ -0,0 +1,4 @@
+// Type promotion happens in C++ while doing method overloading
+// Byte gets promoted to short then int then long etc. (Implicit)
+// Confusion can also occur while matching the data types
+// Explicit type demotion can also occur
